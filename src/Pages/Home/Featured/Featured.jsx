a9b5d64e281a489ef6bca6d496fc965d9049192d
Waseque Arafat <waseque.ndc@gmail.com>
2024-05-04 19:50:24 +0600

    Module-74 done

--- src/Pages/Home/Featured/Featured.jsx
+++ src/Pages/Home/Featured/Featured.jsx
@@ -4,18 +4,18 @@ import "./featured.css";
 
 const Featured = () => {
   return (
-    <div className="featureitems pt-10 my-10 text-white">
+    <div className="featureitems bg-fixed pt-10 my-10 text-white">
       <SectionTitle
         subHeading={"Check it Out "}
         heading={" from our MEnu"}
       ></SectionTitle>
 
-      <div className="md:flex justify-center items-center py-20 px-36 pt-12">
+      <div className="md:flex bg-slate-400 bg-opacity-40 justify-center items-center py-20 px-36 pt-12">
         <div>
           <img src={img} alt="" />
         </div>
 
-        <div className="md:ml-10">
+        <div className="md:ml-10 text-black font-2xl font-semibold">
           <h1 className="uppercase"> March 20, 2023</h1>
           <h2 className="uppercase "> WHERE CAN I GET SOME?</h2>
           <p>
@@ -24,7 +24,9 @@ const Featured = () => {
             Eaque repellat recusandae ad laudantium tempore consequatur
             consequuntur omnis ullam maxime tenetur.
           </p>
-          <button className="btn btn-outline">Read More</button>
+          <button className="btn btn-outline border-0  border-b-4 mt-10 text-white">
+            Read More
+          </button>
         </div>
       </div>
     </div>
