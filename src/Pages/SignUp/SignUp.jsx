fce31136f17afcd204dbffdbe6f0cc6253ad19d0
Waseque Arafat <waseque.ndc@gmail.com>
2024-05-08 09:52:03 +0600

    sign up done

--- src/Pages/SignUp/SignUp.jsx
+++ src/Pages/SignUp/SignUp.jsx
@@ -1,18 +1,17 @@
-import { useContext } from "react";
+import { useForm } from "react-hook-form";
 import img from "../../assets/others/authentication1.png";
 import { Link } from "react-router-dom";
-import { AuthContext } from "../../Provider/AuthProvider";
 
 const SignUp = () => {
-  const { createUser } = useContext(AuthContext);
-  const handleregister = (event) => {
-    event.preventDefault();
-    const form = event.target;
-    const email = form.email.value;
-    const password = form.password.value;
-    const name = form.name.value;
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm();
 
-    console.log(name, email, password);
+  const onSubmit = (data) => {
+    console.log(data);
   };
   return (
     <div className="hero min-h-screen bg-base-200">
@@ -23,13 +22,15 @@ const SignUp = () => {
         {/*  from */}
         {/* TODO: */}
         <div className="card shrink-0 md:w-1/2 max-w-sm shadow-2xl bg-base-100">
-          <form onSubmit={handleregister} className="card-body">
+          {/*   */}
+          <form onSubmit={handleSubmit(onSubmit)} className="card-body">
             <div className="form-control">
               <label className="label">
                 <span className="label-text">Name</span>
               </label>
               <input
                 type="text"
+                {...register("name")}
                 name="name"
                 placeholder="name"
                 className="input input-bordered"
@@ -42,6 +43,7 @@ const SignUp = () => {
               </label>
               <input
                 type="email"
+                {...register("email")}
                 name="email"
                 placeholder="email"
                 className="input input-bordered"
@@ -54,16 +56,12 @@ const SignUp = () => {
               </label>
               <input
                 type="password"
+                {...register("password")}
                 placeholder="password"
                 name="password"
                 className="input input-bordered"
                 required
               />
-              <label className="label">
-                <a href="#" className="label-text-alt link link-hover">
-                  Forgot password?
-                </a>
-              </label>
             </div>
 
             <div className="form-control mt-6">
